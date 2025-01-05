47d2cd5ce12d4da342f133f259a911523dd8d9b8
lior-yossef <lior.yossef@post.runi.ac.il>
2025-01-05 09:57:43 +0200

    fix toString prefix

--- Network.java
+++ Network.java
@@ -115,7 +115,7 @@ public class Network {
 
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
-        String str = "";
+        String str = "Network: ";
 
         for (int i = 0; i < userCount; i++) {
             str += users[i] + "\n";
